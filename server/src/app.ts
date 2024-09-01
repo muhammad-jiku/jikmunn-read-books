42b0591c854237bb74d1f973083ac1200af739a1
muhammad-jiku <muhammadjiku364@gmail.com>
2024-09-01 13:20:37 +0600

    app file init

---
+++ server/src/app.ts
@@ -0,0 +1,31 @@
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import express, { Application, NextFunction, Request, Response } from 'express';
+import httpStatus from 'http-status';
+
+// app init
+const app: Application = express();
+
+// parser and middleware
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// handle not found
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(httpStatus.NOT_FOUND).json({
+    success: false,
+    message: 'Not Found',
+    errorMessages: [
+      {
+        path: req.originalUrl,
+        message: 'API Not Found',
+      },
+    ],
+  });
+
+  next();
+});
+
+export default app;
