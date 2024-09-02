3dcee4c937438c2169a5aa3321403417f4fb8035
muhammad-jiku <muhammadjiku364@gmail.com>
2024-09-02 14:57:25 +0600

    dependencies

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,6 +1,6 @@
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
-import express, { Application, Request } from 'express';
+import express, { Application, NextFunction, Request, Response } from 'express';
 import httpStatus from 'http-status';
 
 // app init
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // handle not found
-app.use((req: Request, res: any, next: response) => {
+app.use((req: Request, res: Response, next: NextFunction) => {
   res.status(httpStatus.NOT_FOUND).json({
     success: false,
     message: 'Not Found',
