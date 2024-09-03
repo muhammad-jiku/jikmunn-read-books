a8eebd7f00cfb3f3812c1e3ac408241b66b8926e
muhammad-jiku <muhammadjiku364@gmail.com>
2024-09-03 23:14:46 +0600

    users controllers func added

---
+++ server/src/app/modules/users/users.controllers.ts
@@ -0,0 +1,48 @@
+import { Request, Response } from 'express';
+import httpStatus from 'http-status';
+import { catchAsync } from '../../../shared/catchAsync';
+import { sendResponse } from '../../../shared/sendResponse';
+import { IUser } from './users.interfaces';
+import { UserServices } from './users.services';
+
+const createCustomer = catchAsync(async (req: Request, res: Response) => {
+  const { customer, ...userData } = req.body;
+  const result = await UserServices.createCustomer(customer, userData);
+
+  sendResponse<IUser>(res, {
+    statusCode: httpStatus.CREATED,
+    success: true,
+    message: 'User created successfully',
+    data: result,
+  });
+});
+
+const createAuthor = catchAsync(async (req: Request, res: Response) => {
+  const { author, ...userData } = req.body;
+  const result = await UserServices.createAuthor(author, userData);
+
+  sendResponse<IUser>(res, {
+    statusCode: httpStatus.CREATED,
+    success: true,
+    message: 'User created successfully',
+    data: result,
+  });
+});
+
+const createAdmin = catchAsync(async (req: Request, res: Response) => {
+  const { admin, ...userData } = req.body;
+  const result = await UserServices.createAdmin(admin, userData);
+
+  sendResponse<IUser>(res, {
+    statusCode: httpStatus.CREATED,
+    success: true,
+    message: 'User created successfully',
+    data: result,
+  });
+});
+
+export const UserControllers = {
+  createCustomer,
+  createAuthor,
+  createAdmin,
+};
