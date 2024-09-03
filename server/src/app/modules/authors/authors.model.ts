1b5424ab6ce455d472ba08ad6eaaba0c71cea422
muhammad-jiku <muhammadjiku364@gmail.com>
2024-09-03 20:52:35 +0600

    author model added

---
+++ server/src/app/modules/authors/authors.model.ts
@@ -0,0 +1,75 @@
+import { model, Schema } from 'mongoose';
+import { gender } from './authors.constants';
+import { IAuthor, IAuthorModel } from './authors.interfaces';
+
+const authorSchema = new Schema<IAuthor, IAuthorModel>(
+  {
+    id: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    name: {
+      type: {
+        firstName: {
+          type: String,
+          required: true,
+        },
+        middleName: {
+          type: String,
+          required: false,
+        },
+        lastName: {
+          type: String,
+          required: true,
+        },
+      },
+      required: true,
+    },
+    gender: {
+      type: String,
+      enum: gender,
+    },
+    dateOfBirth: {
+      type: String,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    contactNo: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    presentAddress: {
+      type: String,
+      required: true,
+    },
+    permanentAddress: {
+      type: String,
+      required: true,
+    },
+    manageBooks: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Book',
+      },
+    ],
+    profileImage: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+    toJSON: {
+      virtuals: true,
+    },
+  },
+);
+
+export const Author: IAuthorModel = model<IAuthor, IAuthorModel>(
+  'Author',
+  authorSchema,
+);
