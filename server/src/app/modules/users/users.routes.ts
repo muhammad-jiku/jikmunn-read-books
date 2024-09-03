862b7950a281c829cb8f76c72b103dbe4dd9bc18
muhammad-jiku <muhammadjiku364@gmail.com>
2024-09-03 23:23:12 +0600

    users routes func added

---
+++ server/src/app/modules/users/users.routes.ts
@@ -0,0 +1,29 @@
+import express from 'express';
+import { validateRequest } from '../../middlewares/validateRequest';
+import { UserControllers } from './users.controllers';
+import { UserValidations } from './users.validations';
+
+const router = express.Router();
+
+router
+  .route('/create-student')
+  .post(
+    validateRequest(UserValidations.createCustomerZodSchema),
+    UserControllers.createCustomer,
+  );
+
+router
+  .route('/create-faculty')
+  .post(
+    validateRequest(UserValidations.createAuthorZodSchema),
+    UserControllers.createAuthor,
+  );
+
+router
+  .route('/create-admin')
+  .post(
+    validateRequest(UserValidations.createAdminZodSchema),
+    UserControllers.createAdmin,
+  );
+
+export const UserRoutes = router;
