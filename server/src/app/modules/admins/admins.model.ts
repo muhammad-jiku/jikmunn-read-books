8e3f3b94975dd50305a6e691107283b627e0e64e
muhammad-jiku <muhammadjiku364@gmail.com>
2024-09-03 21:12:38 +0600

    admin model added

---
+++ server/src/app/modules/admins/admins.model.ts
@@ -0,0 +1,69 @@
+import { model, Schema } from 'mongoose';
+import { gender } from './admins.constants';
+import { IAdmin, IAdminModel } from './admins.interfaces';
+
+const adminSchema = new Schema<IAdmin, IAdminModel>(
+  {
+    id: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    name: {
+      type: {
+        firstName: {
+          type: String,
+          required: true,
+        },
+        middleName: {
+          type: String,
+          required: false,
+        },
+        lastName: {
+          type: String,
+          required: true,
+        },
+      },
+      required: true,
+    },
+    gender: {
+      type: String,
+      enum: gender,
+    },
+    dateOfBirth: {
+      type: String,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    contactNo: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    presentAddress: {
+      type: String,
+      required: true,
+    },
+    permanentAddress: {
+      type: String,
+      required: true,
+    },
+    profileImage: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+    toJSON: {
+      virtuals: true,
+    },
+  },
+);
+
+export const Admin: IAdminModel = model<IAdmin, IAdminModel>(
+  'Admin',
+  adminSchema,
+);
