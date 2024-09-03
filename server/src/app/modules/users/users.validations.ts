d3a966648ba4a74f011edcbeb78c79099c689cb3
muhammad-jiku <muhammadjiku364@gmail.com>
2024-09-03 14:24:02 +0600

    users validations funcs added

---
+++ server/src/app/modules/users/users.validations.ts
@@ -0,0 +1,130 @@
+import { z } from 'zod';
+import { gender } from './users.constants';
+
+const createCustomerZodSchema = z.object({
+  body: z.object({
+    password: z.string().optional(),
+    customer: z.object({
+      name: z.object({
+        firstName: z.string({
+          required_error: 'First name is required',
+        }),
+        middleName: z.string().optional(),
+        lastName: z.string({
+          required_error: 'Last name is required',
+        }),
+      }),
+      gender: z.enum([...gender] as [string, ...string[]], {
+        required_error: 'Gender is required',
+      }),
+      dateOfBirth: z.string({
+        required_error: 'Date of birth is required',
+      }),
+      email: z
+        .string({
+          required_error: 'Email is required',
+        })
+        .email(),
+      contactNo: z.string({
+        required_error: 'Contact number is required',
+      }),
+      presentAddress: z.string({
+        required_error: 'Present address is required',
+      }),
+      permanentAddress: z.string({
+        required_error: 'Permanent address is required',
+      }),
+      wishlist: z.string().optional(),
+      currentlyReading: z.string().optional(),
+      planToRead: z.string().optional(),
+      finishedReading: z.string().optional(),
+      profileImage: z.string().optional(),
+    }),
+  }),
+});
+
+const createAuthorZodSchema = z.object({
+  body: z.object({
+    password: z.string().optional(),
+    author: z.object({
+      name: z.object({
+        firstName: z.string({
+          required_error: 'First name is required',
+        }),
+        middleName: z.string().optional(),
+        lastName: z.string({
+          required_error: 'Last name is required',
+        }),
+      }),
+      gender: z.enum([...gender] as [string, ...string[]], {
+        required_error: 'Gender is required',
+      }),
+      dateOfBirth: z.string({
+        required_error: 'Date of birth is required',
+      }),
+      email: z
+        .string({
+          required_error: 'Email is required',
+        })
+        .email(),
+      contactNo: z.string({
+        required_error: 'Contact number is required',
+      }),
+      presentAddress: z.string({
+        required_error: 'Present address is required',
+      }),
+      permanentAddress: z.string({
+        required_error: 'Permanent address is required',
+      }),
+      manageBooks: z.string().optional(),
+      profileImage: z.string().optional(),
+    }),
+  }),
+});
+
+const createAdminZodSchema = z.object({
+  body: z.object({
+    password: z.string().optional(),
+    admin: z.object({
+      name: z.object({
+        firstName: z.string({
+          required_error: 'First name is required',
+        }),
+        middleName: z.string().optional(),
+        lastName: z.string({
+          required_error: 'Last name is required',
+        }),
+      }),
+      gender: z.enum([...gender] as [string, ...string[]], {
+        required_error: 'Gender is required',
+      }),
+      dateOfBirth: z.string({
+        required_error: 'Date of birth is required',
+      }),
+      email: z
+        .string({
+          required_error: 'Email is required',
+        })
+        .email(),
+      contactNo: z.string({
+        required_error: 'Contact number is required',
+      }),
+      emergencyContactNo: z.string({
+        required_error: 'Emergency contact number is required',
+      }),
+      presentAddress: z.string({
+        required_error: 'Present address is required',
+      }),
+      permanentAddress: z.string({
+        required_error: 'Permanent address is required',
+      }),
+      profileImage: z.string().optional(),
+    }),
+  }),
+});
+
+export const UserValidations = {
+  createCustomerZodSchema,
+  createAuthorZodSchema,
+  createAdminZodSchema,
+};
