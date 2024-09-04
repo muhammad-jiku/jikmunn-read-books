93068121d1ae0cf7f2fa2f00d5464b5f05fb3929
muhammad-jiku <muhammadjiku364@gmail.com>
2024-09-04 11:51:54 +0600

    author validations func added

---
+++ server/src/app/modules/authors/authors.validations.ts
@@ -0,0 +1,29 @@
+import { z } from 'zod';
+import { gender } from './authors.constants';
+
+const updateAuthorZodSchema = z.object({
+  body: z
+    .object({
+      name: z
+        .object({
+          firstName: z.string().optional(),
+          middleName: z.string().optional(),
+          lastName: z.string().optional(),
+        })
+        .strict()
+        .optional(),
+      gender: z.enum([...gender] as [string, ...string[]]).optional(),
+      dateOfBirth: z.string().optional(),
+      email: z.string().email().optional(),
+      contactNo: z.string().optional(),
+      presentAddress: z.string().optional(),
+      permanentAddress: z.string().optional(),
+      manageBooks: z.string().optional(),
+      profileImage: z.string().optional(),
+    })
+    .strict(),
+});
+
+export const AuthorValidations = {
+  updateAuthorZodSchema,
+};
