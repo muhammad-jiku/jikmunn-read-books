391012041e3427644eb4f3788e0abb7715d999c4
muhammad-jiku <muhammadjiku364@gmail.com>
2024-09-04 23:05:30 +0600

    manage books interfaces added

---
+++ server/src/app/modules/manageBooks/manageBooks.interfaces.ts
@@ -0,0 +1,12 @@
+import { Model, Schema } from 'mongoose';
+import { IBook } from '../books/books.interfaces';
+
+export interface IManageBooks {
+  author: Schema.Types.ObjectId; // Reference to the Author
+  books: {
+    book: Schema.Types.ObjectId | IBook; // Reference to the Book or the Book details
+    addedAt: Date; // Optional: Track when the book was added
+  }[];
+}
+
+export type IManageBooksModel = Model<IManageBooks, Record<string, unknown>>;
